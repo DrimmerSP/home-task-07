560df12cdf5627e5cba48e8012214a61dca9e141
Drimmer <drimmerps@yandex.ru>
2023-05-08 13:05:48 +0300

    Init project DZ_07

---
+++ src/main/java/ru/homework/hometask07/HomeTask07Application.java
@@ -0,0 +1,13 @@
+package ru.homework.hometask07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeTask07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeTask07Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/homework/hometask07/HomeTask07ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.homework.hometask07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeTask07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
