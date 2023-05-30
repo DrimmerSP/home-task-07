84cb5adf6541cb01e3dc259a1d40454f39876f49
Drimmer <drimmer88@gmail.com>
2023-05-30 23:53:05 +0300

    Дз_9 Начал делать.

---
+++ src/main/java/ru/homework/hometask07/controller/dto/DirectorDto.java
@@ -0,0 +1,11 @@
+package ru.homework.hometask07.controller.dto;
+
+import java.util.List;
+
+public class DirectorDto {
+    private Integer id;
+    private String directorFIO;
+    private Integer position;
+
+    private List<Integer> filmIDs; //TODO продолжить DTO для всех энтити. И начать мапперы
+}
