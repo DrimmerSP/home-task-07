4ca15db2c1fbd90c494dc32612e918237f3c2dc3
Drimmer <drimmer88@gmail.com>
2023-06-02 00:51:57 +0300

    Дз_9 Продолжение начала по Мапперам

--- src/main/java/ru/homework/hometask07/mapper/FilmMapper.java
+++ src/main/java/ru/homework/hometask07/mapper/FilmMapper.java
@@ -1,7 +1,32 @@
 package ru.homework.hometask07.mapper;
 
 
-public interface FilmMapper {
-    //TODO доделать мапперы
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.homework.hometask07.controller.dto.FilmDto;
+import ru.homework.hometask07.dao.DirectorRepository;
+import ru.homework.hometask07.dao.entity.DirectorEntity;
+import ru.homework.hometask07.dao.entity.FilmEntity;
 
+@Component
+@RequiredArgsConstructor
+public class FilmMapper {
+    //TODO доделать мапперы
+    private final DirectorRepository directorRepository;
+
+    public static FilmDto entityToDto(FilmEntity entity) {
+        return new FilmDto(entity.getId(),
+                entity.getTitle(),
+                entity.getPremierDate(),
+                entity.getCountry(),
+                entity.getGenre(),
+                entity.getDirectors().stream()
+                        .map(DirectorEntity::getId)
+                        .toList()
+        );
+    }
+
+    public FilmEntity dtoToEntity(FilmDto dto) {
+        return FilmEntity.;  //TODO продолжить Маппер
+    }
 }
